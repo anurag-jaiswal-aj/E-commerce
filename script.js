1f5b59791bd13b9e4a02b7a4f3a8b31feaf69f7c
Anurag Jaiswal <janurag582004@gmail.com>
2024-06-17 11:01:05 +0530

    .

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const nav = document.getElementById("navbar");
 if(bar){
     bar.addEventListener('click',() =>{
         nav.classList.add('active');
-    })
+    });
 }
 
 if(close){
     close.addEventListener('click',() =>{
         nav.classList.remove('active');
-    })
+    });
 }
+
+
+
